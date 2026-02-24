4982bb5676220e9b194b255a25eb6cc253597b97
imravi05 <ravishindeshinde12@gmail.com>
2026-02-24 22:50:28 +0530

    auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -5,7 +5,7 @@ export const register = async (req, res, next) => {
     const user = await authService.registerUser(req.body);
     res.status(201).json({ success: true, data: user });
   } catch (error) {
-    res.status(400).send({message:"registration failed", success :false})
+    //res.status(400).send({message:"registration failed", success :false})
       next(error);
   }
 };
