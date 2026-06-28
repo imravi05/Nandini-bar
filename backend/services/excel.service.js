fcf057ef9e347a3d1daeadd5fc9aa69c2b88cd4a
imravi05 <ravishindeshinde12@gmail.com>
2026-06-28 17:44:43 +0530

    excel service

--- backend/services/excel.service.js
+++ backend/services/excel.service.js
@@ -16,7 +16,7 @@ export const generateDailyExcel = async (closingData) => {
 
   // 🟢 TITLE
   sheet.mergeCells("A1:H1");
-  sheet.getCell("A1").value = "OM SAI KRUPA - DAILY SALES REPORT";
+  sheet.getCell("A1").value = "Nandini Bar - DAILY SALES REPORT";
   sheet.getCell("A1").font = { size: 16, bold: true };
   sheet.getCell("A1").alignment = { horizontal: "center" };
 
@@ -30,7 +30,7 @@ export const generateDailyExcel = async (closingData) => {
     "OB",
     "Received",
     "Total",
-    "Parcell",
+    "Parcel",
     "Total",
     "Sale",
     "CB",
